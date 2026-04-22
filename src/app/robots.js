f32f4c6624d4fe85d02253992c3a2d05a71e788b
Sachin <100507127+sachinpal11@users.noreply.github.com>
2026-04-22 07:52:52 +0530

    feat: add robots.txt configuration for search engine indexing

--- src/app/robots.js
+++ src/app/robots.js
@@ -4,7 +4,7 @@ export default function robots() {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/_next/'],
+        disallow: ['/api/'],
       },
     ],
     sitemap: 'https://onthegopdf.vercel.app/sitemap.xml',
